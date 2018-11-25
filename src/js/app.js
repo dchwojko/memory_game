540556f7275ad69f74dc4be388ad6666aa667004
Don <dchwojko@mac.com>
2018-11-25 16:14:56 -0600

    can select/show/hide cards

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,54 @@
 /*
  * Create a list that holds all of your cards
  */
-var cards = document.querySelectorAll('.card');
+
 var hasWon = false;
 const maxMoves = 3;
 let remainingMoves = 3;
 
+var cards = getCards();
+var selectedCards = [];
 var openCards = [];
+var indexes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
-var restartButton = document.querySelector('.restart');
-restartButton.addEventListener('click', function() {
+var restartButton;
+
+handleRestart();
+
+function setupRestartButton() {
+    restartButton = document.querySelector('.restart');
+    restartButton.addEventListener('click', function() {
     handleRestart();
-});
+    });
+}
 
+function getCards() {
+    return document.querySelectorAll('.card');
+}
+
+
+
+/* TO DO
 var scorePanel = document.querySelector('.score-panel');
 var stars = document.querySelector('.stars');
 var moves = document.querySelector('.moves');
 
 function updateMoves() {
     
+}
+*/
+
+function deal() {
+    //console.log("dealing");
+    const deck = document.querySelector('.deck');
+    deck.innerHTML = "";
+    indexes = shuffle(indexes);
+
+    for (index of indexes) {
+      //  console.log(cards[index]);
+        deck.appendChild(cards[index]);
+    }
+    cards = getCards();
 }
 
 /*
@@ -28,8 +58,18 @@ function updateMoves() {
  *   - add each card's HTML to the page
  */
 
- for (let card of cards) {
-     addEventListenerForCard(card);
+ 
+
+ function addEventListenerForDeck() {
+     for (card of cards) {
+         addEventListenerForCard(card);
+     }
+ }
+
+ function removeEventListenerForDeck() {
+     for (card of cards) {
+         removeEventListenerForCard(card);
+     }
  }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -49,16 +89,38 @@ function shuffle(array) {
 
 function resetCards() {
     for (let card of cards) {
-        card.classList = "card"
+        removeEventListenerForCard(card);
+        addEventListenerForCard(card);
+        setCardClassListToCard(card);  
     }
 }
 
+function setCardClassListToCard(card) {
+    card.classList = "card";
+    //console.log(card.classList);
+}
+
+function setCardClassListToShow(card) {
+    card.classList.toggle('open');
+    card.classList.toggle('show');
+    //console.log(card.classList);
+}
+
+function setCardClassListToMatch(card) {
+    card.classList.toggle('open');
+    card.classList.toggle('show');
+    card.classList.toggle('match');
+    //console.log(card.classList);
+}
+
 function handleRestart() {
-    resetMoves();
+    //resetMoves();
     resetCards();
+    deal();
 }
 
 function resetMoves() {
+    // TO DO
     remainingMoves = maxMoves;
 }
 
@@ -76,47 +138,44 @@ function resetMoves() {
  */
 
  function handleCardClick(card) {
-     console.log('card was clicked');
-     if (openCards.length < 2) {
-        openCards.push(card);
-        card.classList.toggle("open");
-        card.classList.toggle("show");
-        removeEventListenerForCard(card);
-        if (openCards.length == 2) {
-            // TO DO: if match
-            console.log(card);
-            const first = openCards[0].innerHTML;
-            const second = openCards[1].innerHTML;
-            console.log(first);
-            console.log(second);
-            
-            if (first == second) {
-                for (card of openCards) {
-                    card.classList.toggle("open");
-                    card.classList.toggle("show");
-                    card.classList.toggle("match");
-                    removeEventListenerForCard(card);
-                }
-                openCards = [];
-
-            } else {
-                // TO DO: not match
-                for (card of openCards) {
-                    card.classList.toggle("open");
-                    card.classList.toggle("show");
-                    addEventListenerForCard(card);    
-                }
-                openCards = [];
-                decrementNumberOfMoves();
-                isGameOver();
-
-            }
-        } else {
-            console.log('to do - less than 2 cards selected');
-        }
+     if (openCards.includes(card) || selectedCards.includes(card)) {
+         return;
      } else {
+        if (selectedCards.length < 2) {
+            setCardClassListToShow(card);
+            selectedCards.push(card);
+        } else {
+            if (selectedCards[0].innerHTML == selectedCards[1].innerHTML) {
+                updateMatchingCards(selectedCards);
+            } else {
+                updateMismatchingCards(selectedCards);
+            }
+            selectedCards = [];
+        }
+     }
+     checkGameStatus();
+ }
 
-     } 
+function updateMatchingCards(matchCards) {
+    for (matchCard of matchCards) {
+        setCardClassListToMatch(matchCard);
+        openCards.push(matchCard);
+    }
+}
+
+function updateMismatchingCards(mismatchCards) {
+    for (mismatchCard of mismatchCards) {
+        setCardClassListToCard(mismatchCard);
+    }
+}
+
+ function checkGameStatus() {
+     if (openCards.length == cards.length) {
+         console.log('you won!');
+     }
+     if (remainingMoves == 0) {
+         console.log('you lost!');
+     }
  }
 
  function isGameOver() {
@@ -131,13 +190,16 @@ function resetMoves() {
  }
 
  function addEventListenerForCard(card) {
-    card.addEventListener('click', function() {
-        handleCardClick(card)
+     //console.log('adding event listener');
+     card.addEventListener('click', function() {
+        handleCardClick(card);
     });
  }
 
  function removeEventListenerForCard(card) {
-    card.removeEventListener('click', function() {
-        handleCardClick(card)
+     //console.log('removing event listener');
+     card.removeEventListener('click', function() {
+        handleCardClick(card);
     });
  }
+
